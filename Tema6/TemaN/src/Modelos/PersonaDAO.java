f0d760d2c43e9d8b33b7383d588dbfc851630499
MarggioRV <marggiorobles@hotmail.com>
2026-02-11 13:36:14 +0100

    ++

--- Tema6/TemaN/src/Modelos/PersonaDAO.java
+++ Tema6/TemaN/src/Modelos/PersonaDAO.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import Actividades6.Conexion;
 
 // public class PersonaDAO {
 //     public List<Persona> listarPersonas() {
