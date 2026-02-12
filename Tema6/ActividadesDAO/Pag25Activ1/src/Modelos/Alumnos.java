e41888e2aa4a75efd9297a85467eada3dd5df65e
MarggioRV <marggiorobles@hotmail.com>
2026-02-12 01:16:41 +0100

    1+

---
+++ Tema6/ActividadesDAO/Pag25Activ1/src/Modelos/Alumnos.java
@@ -0,0 +1,74 @@
+package Modelos;
+
+public class Alumnos {
+
+    //Atributos
+
+    private int numMatricula;
+    private String fecha_nac;
+    private String nombre;
+    private String Ape1;
+    private String Ape2;
+    private String telefono;
+    
+    //Constructores
+
+    public Alumnos(int numMatricula, String fecha_nac, String nombre, String ape1, String ape2, String telefono) {
+        this.numMatricula = numMatricula;
+        this.fecha_nac = fecha_nac;
+        this.nombre = nombre;
+        Ape1 = ape1;
+        Ape2 = ape2;
+        this.telefono = telefono;
+    }
+
+    //Getter y Setters
+
+    public int getNumMatricula() {
+        return numMatricula;
+    }
+
+    public void setNumMatricula(int numMatricula) {
+        this.numMatricula = numMatricula;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nac;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nac = fecha_nacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApe1() {
+        return Ape1;
+    }
+
+    public void setApe1(String ape1) {
+        Ape1 = ape1;
+    }
+
+    public String getApe2() {
+        return Ape2;
+    }
+
+    public void setApe2(String ape2) {
+        Ape2 = ape2;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
---
+++ Tema6/ActividadesDAO/Pag25Activ1/src/Vista/AlumnosView.java
@@ -0,0 +1,78 @@
+package Vista;
+
+import java.util.List;
+import java.util.Scanner;
+
+import Modelos.Alumnos;
+
+
+public class AlumnosView {
+    private Scanner sc = new Scanner(System.in);
+
+    public int mostrarMenu() {
+        System.out.println("\n--- MENÚ PERSONAS ---");
+        System.out.println("1. Listar Alumnos");
+        System.out.println("2. Insertar Alumnos");
+        System.out.println("3. Actualizar Alumnos");
+        System.out.println("4. Eliminar Alumnos");
+        System.out.println("0. Salir");
+        System.out.print("Opción: ");
+        System.out.println();
+        return sc.nextInt();
+    }
+
+    public void mostrarAlumnos(List<Alumnos> alumnos) {
+        System.out.println("\nLISTADO:");
+        for (Alumnos a : alumnos) {
+            System.out.println(a.getNombre() 
+                + " - " + a.getApe1()
+                + " - " + a.getApe2()
+                + " - " + a.getNumMatricula());
+        }
+    }
+
+    public Alumnos pedirNuevoAlumno() {
+        sc.nextLine();
+        System.out.print("#Matricula: ");
+        int numMatricula = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Fecha de Nacimiento: ");
+        String fecha_nac = sc.nextLine();
+        System.out.print("Nombre: ");
+        String nombre = sc.nextLine();
+        System.out.print("Apellido1: ");
+        String Ape1 = sc.nextLine();
+        System.out.print("Apellido2: ");
+        String Ape2 = sc.nextLine();
+        System.out.print("telefono: ");
+        String telefono = sc.nextLine();
+        return new Alumnos(numMatricula, fecha_nac, nombre, Ape1, Ape2, telefono);
+    }
+
+    public Alumnos pedirAlumnoActualizar() {
+        System.out.print("#Matricula a modificar: ");
+        int numMatricula = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Nuevo fecha_nacimiento: ");
+        String nombre = sc.nextLine();
+        System.out.print("Nuevo nombre: ");
+        String fecha_nac = sc.nextLine();
+        System.out.print("Nuevo apellido1: ");
+        String Ape1 = sc.nextLine();
+        System.out.print("Nuevo apellido2: ");
+        String Ape2 = sc.nextLine();
+        System.out.print("Nuevo telefono: ");
+        String telefono = sc.nextLine();
+        return new Alumnos(numMatricula, fecha_nac, nombre, Ape1, Ape2, telefono);
+    }
+
+    public int pedirNumMatriculaEliminar() {
+        System.out.print("#Matricula a eliminar: ");
+        return sc.nextInt();
+    }
+
+    public void mostrarMensaje(String msg) {
+        System.out.println(msg);
+    }
+}
+
