46b71970a9d915a3a44d4eade1b6dca9b395e9f4
MarggioRV <marggiorobles@hotmail.com>
2026-02-23 23:55:53 +0100

    +2

--- Tema6.4-ProyectoGUI-Ejm/src/Ejemplo1.java
+++ Tema6.4-ProyectoGUI-Ejm/src/Ejemplo1.java
@@ -2,10 +2,12 @@ import javax.swing.*;
 
 public class Ejemplo1 extends JFrame {
     private JPanel Margg;
+    private JTextField textField1;
+    private JButton button1;
 
     public Ejemplo1() {
-
         setTitle("Hey...........");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setContentPane(Margg);
         setVisible(true);
         setSize(500, 500);
