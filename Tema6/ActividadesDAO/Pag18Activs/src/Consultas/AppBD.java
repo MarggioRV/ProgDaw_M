399afac0b447668c64780c5101a6e5a333098496
MarggioRV <marggiorobles@hotmail.com>
2026-03-08 01:27:00 +0100

    Agregando las consultas faltantes

--- Tema6/ActividadesDAO/Pag18Activs/src/Consultas/AppBD.java
+++ Tema6/ActividadesDAO/Pag18Activs/src/Consultas/AppBD.java
@@ -18,9 +18,11 @@ public class AppBD {
         // }
         listarPersonas("Spain","Lakers");
         NuevoRegistro(700, "Petro", "Rusia", "10-1", 30, "A", "Warriors");
-        
         // System.out.println("...");
         // actulizarJugador();
+        // Listados extra
+        listarMaxPuntosPorTemp();
+        listarEquiposLocales();
     }
     
     public static void crearPersona(String nombre, String email) {
@@ -54,6 +56,7 @@ public class AppBD {
     //     }
     // }
 
+        /* Muestra los jugadores españoles que son de los Lakers */
         public static void listarPersonas(String Procedencia,String equipo) {
         String sql = "SELECT codigo, nombre, Procedencia, Nombre_equipo FROM jugadores where Procedencia=? and Nombre_Equipo=?";
         try  {
@@ -75,7 +78,6 @@ public class AppBD {
     }
 
     /* Modifica el jugador 666 para que ponga bien su Procedencia */
-
     public static void actulizarJugador() {
         String sql = "UPDATE Jugadores SET Procedencia = ? WHERE codigo = ?";
 
@@ -91,7 +93,6 @@ public class AppBD {
     }
 
     /* Inserta un registro en jugadores y bórralo */
-
     public static void NuevoRegistro(int codigo, String nombre, String Procedencia, String Altura, int Peso, String Posicion, String Nombre_Equipo) {
         String sql = "INSERT INTO jugadores (codigo, nombre, Procedencia, Altura, Peso, Posicion, Nombre_equipo) VALUES (?, ?, ?, ?, ?, ?, ?)";
         try (Connection conn = Conexion.getConnection();
@@ -111,6 +112,48 @@ public class AppBD {
         }
     }
 
+    //1.4
+    public static void listarEquiposLocales(){
+        String sql = "SELECT DISTINCT Equipo_Local FROM Partidos WHERE Puntos_local > 100 AND puntos_local > Puntos_Visitante AND Temporada = ?";
+        try (Connection conn = Conexion.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);) { 
+
+            ps.setString(1, "02/03");
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                System.out.println("EQUIPO LOCAL: " + rs.getString("Equipo_local"));
+            }
+        } catch (Exception e) {
+            System.out.println("Error al leer: " + e.getLocalizedMessage());
+        }
+    }
+
+    //1.5
+    public static void listarMaxPuntosPorTemp(){
+        String sql =
+        "SELECT JUGADORES.nombre, ESTADISTICAS.temporada, ESTADISTICAS.puntos_por_partido FROM JUGADORES, ESTADISTICAS" +
+        "WHERE ESTADISTICAS.jugador = JUGADORES.C" +
+        "AND (ESTADISTICAS.temporada, ESTADISTICAS.puntos_por_partido) IN" +
+        "(SELECT temporada, MAX(puntos_por_partido) FROM ESTADISTICAS" +
+        "GROUP BY temporada" +
+        "ORDER BY ESTADISTICAS.temporada";
+
+        try (Connection conn = Conexion.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery()) {
+            
+            while (rs.next()) {
+                System.out.println(
+                    rs.getString("nombre") + "-" +
+                    rs.getString("temporada") + "-" +
+                    rs.getFloat("puntos_por_partido")
+                );
+            }
+        } catch (Exception e) {
+            System.out.println("Error en la lectura: " + e.getMessage());
+        }
+    }
 }
 
 
