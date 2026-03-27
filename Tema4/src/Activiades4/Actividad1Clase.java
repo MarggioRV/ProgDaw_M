6aec69208f913a88cb640bb8eccddcaee3544e63
MarggioRV <marggiorobles@hotmail.com>
2026-03-28 00:26:56 +0100

    C

--- Tema4/src/Activiades4/Actividad1Clase.java
+++ Tema4/src/Activiades4/Actividad1Clase.java
@@ -5,7 +5,7 @@ import Activiades4.actividadesClases.Cuenta;
 
 public class Actividad1Clase {
     public static void main(String[] args) {
-        Persona cliente=new Persona("Luis","123456789A","31/1/2024","mi casa",true,true);
+        Persona cliente=new Persona("Pedro","123456789A","31/1/2024","mi casa",true,true);
         Cuenta cuentaBancaria = new Cuenta("432552435245243545",2000,cliente);
 
         cliente.imprimePersona();
